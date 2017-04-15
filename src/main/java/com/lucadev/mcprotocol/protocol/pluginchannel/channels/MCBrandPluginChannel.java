d1214b90529e02d1d54732b5f69da96bf281705a
Luca Camphuisen <luca.camphuisen@hva.nl>
2017-04-15 19:41:56 +0200

    Improved and added JavaDoc

--- src/main/java/com/lucadev/mcprotocol/protocol/pluginchannel/channels/MCBrandPluginChannel.java
+++ src/main/java/com/lucadev/mcprotocol/protocol/pluginchannel/channels/MCBrandPluginChannel.java
@@ -12,13 +12,23 @@ import static com.lucadev.mcprotocol.protocol.VarHelper.readString;
 import static com.lucadev.mcprotocol.protocol.VarHelper.writeString;
 
 /**
+ * Two-way plugin message to announce client/server implementation name.
+ *
  * @author Luca Camphuisen < Luca.Camphuisen@hva.nl >
  */
 public class MCBrandPluginChannel extends PluginChannel {
 
-    private Logger logger = LoggerFactory.getLogger(MCBrandPluginChannel.class);
+    /**
+     * System logger
+     */
+    private final static Logger logger = LoggerFactory.getLogger(MCBrandPluginChannel.class);
 
-    private String clientName = "vanilla";
+    /**
+     * Client implementation name to send to the server.
+     */
+    private static final String CLIENT_IMPLEMENTATION = "vanilla";
+
+    private String serverImplementation;
 
     /**
      * Constructor setting the channel name
@@ -28,24 +38,32 @@ public class MCBrandPluginChannel extends PluginChannel {
     }
 
     /**
-     * Handle the bytes
-     *
-     * @param is
+     * Handle custom data payload
+     * @param is stream containing all payload data.
+     * @throws IOException when something goes wrong while handling payload data.
      */
     @Override
     public void readPayload(DataInputStream is) throws IOException {
-        String clientName = readString(is);
-        logger.info("Received server implementation: " + clientName);
+        serverImplementation = readString(is);
+        logger.info("Received server implementation: " + serverImplementation);
     }
 
     /**
-     * Data to write
-     *
-     * @param dos
+     * @return name of the server implementation.
+     *          Null if this channel was not supported by the server or the channel hasn't been loaded yet.
+     */
+    public String getServerImplementationName() {
+        return serverImplementation;
+    }
+
+    /**
+     * When registering your channel to the server write your data payload through this method.
+     * @param dos stream for custom data payload.
+     * @throws IOException when something goes wrong while writing payload data.
      */
     @Override
     public void registerToServer(DataOutputStream dos) throws IOException {
-        writeString(dos, clientName);
+        writeString(dos, CLIENT_IMPLEMENTATION);
     }
 
 }
