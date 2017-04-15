5dec91d1c71a54c75e73c1f50067fe0347193de8
Luca Camphuisen <luca.camphuisen@hva.nl>
2017-04-15 11:55:36 +0200

    Added JavaDoc

--- src/main/java/com/lucadev/mcprotocol/auth/offline/OfflineModeSessionProvider.java
+++ src/main/java/com/lucadev/mcprotocol/auth/offline/OfflineModeSessionProvider.java
@@ -12,16 +12,26 @@ import java.io.IOException;
  */
 public class OfflineModeSessionProvider extends SessionProvider {
 
+    /**
+     * Ignores legit authentication and simply returns a session with the given username that can only be used for Offline mode
+     * @param email for offline-mode this requires to be your player name and not an e-mail address.
+     * @param password skipped completely since we are not actually authenticating.
+     * @return a session that contains empty data except the given username.
+     * @throws IOException will be thrown when something unexpected happens during authentication.
+     *                      An example would be that the auth servers could be down or you lost your internet connection.
+     */
     @Override
     public Session authenticate(String email, String password) throws IOException {
         return new OfflineSession(email);
     }
 
     /**
-     * Authenticate to mojang auth servers.
+     * Authenticate to a service so that we are able to join servers.
+     * This can be completely skipped by offline-mode servers because we do not have the required session data to authenticte.
+     * Offline-mode servers simply skip this step.
      *
-     * @param session
-     * @param hash
+     * @param session the user session
+     * @param hash the hash generated from earlier steps. This hash is used to verify the user.
      */
     @Override
     public void authenticateServer(Session session, String hash) {
