0de4857792dd70375df8aae30bf2a612f1bc1c55
Luca Camphuisen <luca.camphuisen@hva.nl>
2017-04-17 17:18:35 +0200

    Refactor ServerJoinable -> ServerLogin

--- src/main/java/com/lucadev/mcprotocol/bots/PlayBot.java
+++ src/main/java/com/lucadev/mcprotocol/bots/PlayBot.java
@@ -2,9 +2,9 @@ package com.lucadev.mcprotocol.bots;
 
 /**
  * Interface offering more decoupling functionality for bots.
- * Extends functionality of ServerJoinable and SessionAuthentication which are all required,
+ * Extends functionality of ServerLogin and SessionAuthentication which are all required,
  *
  * @author Luca Camphuisen < Luca.Camphuisen@hva.nl >
  */
-public interface PlayBot extends Bot, ServerJoinable, SessionAuthentication {
+public interface PlayBot extends Bot, ServerLogin, SessionAuthentication {
 }
--- src/main/java/com/lucadev/mcprotocol/bots/ServerLogin.java
+++ src/main/java/com/lucadev/mcprotocol/bots/ServerLogin.java
@@ -3,12 +3,12 @@ package com.lucadev.mcprotocol.bots;
 import java.io.IOException;
 
 /**
- * Interface used by bots to add server join functionality.
- * This functionality means completing a succesful login.
+ * Interface used by bots to add server login functionality.
+ * This functionality means completing a successful login.
  *
  * @author Luca Camphuisen < Luca.Camphuisen@hva.nl >
  */
-public interface ServerJoinable {
+public interface ServerLogin {
 
     /**
      * Join the server at the configured host and port
